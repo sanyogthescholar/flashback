5b0175b504e77f7798f6a23846cc2a11e7981925
Sanyog Jain <sanyogjain5678@gmail.com>
2023-10-29 17:04:38 +0530

    Add requirements.txt

--- image_caption.py
+++ image_caption.py
@@ -10,19 +10,3 @@ def caption_image(path):
     inputs = processor(raw_image, return_tensors="pt")
     out = model.generate(**inputs)
     return processor.decode(out[0], skip_special_tokens=True)
-
-#img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
-#raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-
-# conditional image captioning
-"""text = "a photography of"
-inputs = processor(raw_image, text, return_tensors="pt")
-
-out = model.generate(**inputs)
-print(processor.decode(out[0], skip_special_tokens=True))"""
-
-# unconditional image captioning
-#inputs = processor(raw_image, return_tensors="pt")
-
-#out = model.generate(**inputs)
-#print(processor.decode(out[0], skip_special_tokens=True))
